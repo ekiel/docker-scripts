d32932e7d2a5c6ab1022b2c2ad426563c81781cc
ekiel <eric.kiel@collideoscope.org>
2018-08-28 19:19:07 -0500

    adding keepalived scripts

---
+++ keepalived-backup3.sh
@@ -0,0 +1,9 @@
+docker run --name keepalived \
+--cap-add=NET_ADMIN \
+--net=host \
+-d \
+-e PRIORITY=100 \
+-e UNICAST_PEERS='192.168.0.31 192.168.0.32 192.168.0.33' \
+-e VIRTUAL_IPS=192.168.0.30/24 \
+-e PASSWORD=sekret \
+rvben/rpi-keepalived
