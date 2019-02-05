eb31ae2617aed9e655bd7604d8fa8267fdbcb33e
Eric Kiel <eric.kiel@collideoscope.org>
2019-02-05 11:17:16 -0600

    updated syncthing config - paths

--- syncthing-config.sh
+++ syncthing-config.sh
@@ -1,8 +1,8 @@
-docker container create \
+docker run -d \
 --name syncthing \
 --publish 22000:22000 \
 --publish 8384:8384 \
 --publish 21027:21027/udp \
--v /nfs/dockerconfig/syncthing-single/:/config \
+-v /nfs/dockerconfig/syncthing/:/config \
 -v /nfs/data/documents/corporate_files/:/data \
 lsioarmhf/syncthing
